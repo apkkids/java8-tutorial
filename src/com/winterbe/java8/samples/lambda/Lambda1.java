4b8cbbf60860371c1e5d9a3adb388f3c60e29faa
Alex Wang <apkkids@163.com>
2019-05-24 17:36:14 +0800

    change some code.

--- src/com/winterbe/java8/samples/lambda/Lambda1.java
+++ src/com/winterbe/java8/samples/lambda/Lambda1.java
@@ -20,30 +20,35 @@ public class Lambda1 {
                 return b.compareTo(a);
             }
         });
+        System.out.println("1:"+names);
 
-        Collections.sort(names, (String a, String b) -> {
-            return b.compareTo(a);
-        });
+//        Collections.sort(names, (String a, String b) -> {
+//            return b.compareTo(a);
+//        });
+        Collections.sort(names,(String a,String b)->{return -b.compareTo(a);});
+        System.out.println("2:"+names);
 
         Collections.sort(names, (String a, String b) -> b.compareTo(a));
+        System.out.println("3:"+names);
 
         Collections.sort(names, (a, b) -> b.compareTo(a));
-
-        System.out.println(names);
+        System.out.println("4:"+names);
 
         names.sort(Collections.reverseOrder());
+        System.out.println("5:"+names);
 
-        System.out.println(names);
+        names.sort(Comparator.naturalOrder());
+        System.out.println("6:"+names);
 
         List<String> names2 = Arrays.asList("peter", null, "anna", "mike", "xenia");
         names2.sort(Comparator.nullsLast(String::compareTo));
-        System.out.println(names2);
+        System.out.println("7:"+names2);
 
         List<String> names3 = null;
 
         Optional.ofNullable(names3).ifPresent(list -> list.sort(Comparator.naturalOrder()));
 
-        System.out.println(names3);
+        System.out.println("8:"+names3);
     }
 
 }
